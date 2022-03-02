989f506354028675c3a0c6cdd0961de6cf3c2f82
Daniel F. Rivera C <tutormanizales17@censa.edu.co>
2022-03-01 20:26:36 -0500

    Create pages for MVP

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+/**
+ * About component
+ * @returns {JSX.Element}
+ */
+export const About = () => (
+  <DefaultLayout>
+    <h1>About page</h1>
+  </DefaultLayout>
+);
+
+export default About;
---
+++ src/components/pages/PokemonDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+/**
+ * Pokemon details component
+ * @returns
+ */
+export const PokemonDetails = () => (
+  <DefaultLayout>
+    <h1>Pokemon detail page</h1>
+  </DefaultLayout>
+);
+
+export default PokemonDetails;
