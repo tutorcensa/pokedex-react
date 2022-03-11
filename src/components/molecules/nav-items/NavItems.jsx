7058884de4909df4bee99face5129217dc0f334e
Daniel F. Rivera C <tutormanizales17@censa.edu.co>
2022-03-10 22:16:14 -0500

    Define nav links style

--- src/components/molecules/nav-items/NavItems.jsx
+++ src/components/molecules/nav-items/NavItems.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavItems.css';
 
 /**
  * Navigation items component
  * @returns {JSX.Element}
  */
 const NavItems = () => (
-  <div>
-    <Link to='/about'>Acerca de</Link>
+  <div className='m-nav-items'>
+    <Link className='m-nav-items__item' to='/about'>
+      Acerca de
+    </Link>
   </div>
 );
 
