135ae324b8b29fa8dfc463a855bcb98d8361b2f8
Daniel F. Rivera C <tutormanizales17@censa.edu.co>
2022-03-29 22:19:19 -0500

    Partially define style for PokemonView component

--- src/components/molecules/pokemon-view/PokemonView.jsx
+++ src/components/molecules/pokemon-view/PokemonView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PokemonTitle from 'components/atoms/pokemon-title/PokemonTitle';
 import PokemonImageContainer from 'components/atoms/pokemon-image-container/PokemonImageContainer';
 import PokemonStats from 'components/atoms/pokemon-stats/PokemonStats';
+import './PokemonView.css';
 
 const PokemonView = (props) => (
   <section className='m-pokemon-view'>
